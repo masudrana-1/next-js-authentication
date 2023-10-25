e59073e7bc143d673fd29f034709c6edba8ea757
Masud Rana <masudrana58855@gmail.com>
2023-10-25 11:13:23 +0600

    create protected route and implement in profile route

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,9 @@ import { GoogleOutlined, GithubOutlined } from "@ant-design/icons";
 import Head from "next/head";
 import styles from "@/styles/Login.module.css";
 import { signIn } from "next-auth/react";
+
+
+
 const LoginPage = () => {
   return (
     <div>
@@ -12,7 +15,11 @@ const LoginPage = () => {
       <div className={styles.form}>
         <h3>LOGIN</h3>
         <div className={styles.social_icons}>
-          <GoogleOutlined />
+          <GoogleOutlined onClick={()=>signIn("google", {
+            callbackUrl: "http://localhost:3000/"
+          })}/>
+
+
           <GithubOutlined onClick={()=>signIn("github", {
             callbackUrl: "http://localhost:3000/"
           })}/>
